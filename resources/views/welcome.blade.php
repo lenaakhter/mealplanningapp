dce1cfc2aa7af9f9ad7cff339eac98478f21a185
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2024-03-06 19:34:15 +0000

    Home screen banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,19 @@
 
 @section('content')
 
-
-<h1>Hello!</h1>
-
-
+<div class="bannertitle">
+    <div class="bannercontents">
+        <div class="bannertext">
+            <h1>Create Your Custom Meal Plan Today</h1>
+        </div>
+        <div class="bannerbutton">
+            <a href="/mealplan">
+                <button>
+                    Create Now
+                </button>
+            </a>
+        </div>
+    </div>
+</div>
 
 @endsection
