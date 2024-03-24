9fea150a87ad88d9bd9719aea8ecba7ded8e2ea5
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2024-03-24 16:29:27 +0000

    meal plans table created

---
+++ database/migrations/2024_03_24_162754_create_meal_plans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meal_plans', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meal_plans');
+    }
+};
