a53b594f8684659232bda9979a63ea05449b6f9b
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2024-03-24 16:42:52 +0000

    nutritional needs table created

---
+++ database/migrations/2024_03_24_164228_create_nutritional_needs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nutritional_needs', function (Blueprint $table) {
+            $table->id();
+            $table->string('age_range');
+            $table->string('gender');
+            $table->integer('calories');
+            $table->decimal('protein_g', 5, 2);
+            $table->decimal('carbohydrates_g', 5, 2);
+            $table->decimal('fats_g', 5, 2);
+            $table->timestamps();
+        });
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nutritional_needs');
+    }
+};
