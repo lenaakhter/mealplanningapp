7496bc404fb533dabe8df707656c4531a9ab4e49
lenaakhter <116122172+lenaakhter@users.noreply.github.com>
2024-04-21 02:08:09 +0100

    dash buttons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,54 @@
 
     </div>
     <div class="dashmainnav">
-
+        <div class="monlink">
+            <a href="/mealplan?day=Monday" class="square-link">
+                <div>Monday</div>
+            </a>
+        </div>
+        <div class="tuelink">
+            <a href="/mealplan?day=Tuesday" class="square-link">
+                <div>Tuesday</div>
+            </a>
+        </div>
+        <div class="wedlink">
+            <a href="/mealplan?day=Wednesday" class="square-link">
+                <div>Wednesday</div>
+            </a>
+        </div>
+        <div class="thurlink">
+            <a href="/mealplan?day=Thursday" class="square-link">
+                <div>Thursday</div>
+            </a>
+        </div>
+        <div class="frilink">
+            <a href="/mealplan?day=Friday" class="square-link">
+                <div>Friday</div>
+            </a>
+        </div>
+        <div class="satlink">
+            <a href="/mealplan?day=Saturday" class="square-link">
+                <div>Saturday</div>
+            </a>
+        </div>
+        <div class="sunlink">
+            <a href="/mealplan?day=Sunday" class="square-link">
+                <div>Sunday</div>
+            </a>
+        </div>
+        <div class="recipesdash">
+            <a href="/recipes" class="square-link">
+                <div>Recipes</div>
+            </a>
+        </div>
+        <div class="logoutdash">
+            <a href="" class="square-link">
+                <div>Logout</div>
+            </a>
+        </div>
     </div>
     <div class="dashhelplinks">
-        
+
     </div>
 </div>
 
